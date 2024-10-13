64af30d6e9a595b2c870c9821ef577d989ca9595
renato matos <renato.matos79@gmail.com>
2024-10-13 23:44:38 +0100

    removing dup resources

--- terraform/rest-api-proxy-resource-1.tf
+++ terraform/rest-api-proxy-resource-1.tf
@@ -47,54 +47,5 @@ resource "aws_api_gateway_integration" "api_events_integration" {
   }
 
   # Pass through all the remaining client requests
-  passthrough_behavior = "WHEN_NO_TEMPLATES"
-}
-
-#####################################
-# Proxy Resource 2: api-token/{proxy+}
-#####################################
-
-# Create the /api-token resource
-resource "aws_api_gateway_resource" "api_token" {
-  rest_api_id = aws_api_gateway_rest_api.udemy_proxy_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.udemy_proxy_api_gateway.root_resource_id
-  path_part   = "api-token"
-}
-
-# Create the /api-token/{proxy+} child resource
-resource "aws_api_gateway_resource" "api_token_proxy" {
-  rest_api_id = aws_api_gateway_rest_api.udemy_proxy_api_gateway.id
-  parent_id   = aws_api_gateway_resource.api_token.id
-  path_part   = "{proxy+}"
-}
-
-# Define the ANY method for /api-token/{proxy+}
-resource "aws_api_gateway_method" "api_token_any_method" {
-  rest_api_id   = aws_api_gateway_rest_api.udemy_proxy_api_gateway.id
-  resource_id   = aws_api_gateway_resource.api_token_proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-
-  # Define the required headers
-  request_parameters = {
-    "method.request.header.Authorization" = true
-  }
-}
-
-# Integration for the ANY method on /api-token/{proxy+}
-resource "aws_api_gateway_integration" "api_token_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.udemy_proxy_api_gateway.id
-  resource_id             = aws_api_gateway_resource.api_token_proxy.id
-  http_method             = aws_api_gateway_method.api_token_any_method.http_method
-  integration_http_method = "ANY"
-  type                    = "HTTP"
-  uri                     = "http://44.196.119.69:8080/api/{proxy}"
-
-  # Map only the Authorization header and path to the backend
-  request_parameters = {
-    "integration.request.header.Authorization" = "method.request.header.Authorization"
-    "integration.request.path.proxy"           = "method.request.path.proxy"
-  }
-
   passthrough_behavior = "WHEN_NO_MATCH"
 }
